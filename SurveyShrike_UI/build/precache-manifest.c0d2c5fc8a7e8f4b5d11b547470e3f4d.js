b321234b674204b85715ed86d20e2dd1b4b3494f
Hemanth Vangala <hemanth@Hemanths-MacBook-Pro.local>
2019-10-20 19:42:51 +0530

    pushing build files

---
+++ SurveyShrike_UI/build/precache-manifest.c0d2c5fc8a7e8f4b5d11b547470e3f4d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f9eb8b47721a97c892",
+    "url": "./static/css/main.fbbcf139.chunk.css"
+  },
+  {
+    "revision": "a2f9eb8b47721a97c892",
+    "url": "./static/js/main.a2f9eb8b.chunk.js"
+  },
+  {
+    "revision": "7dca62c24f2798d6cdd0",
+    "url": "./static/css/1.b82c1873.chunk.css"
+  },
+  {
+    "revision": "7dca62c24f2798d6cdd0",
+    "url": "./static/js/1.7dca62c2.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "./static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "./static/media/1.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "./static/media/2.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "./static/media/3.026cb7af.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "./static/media/4.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "./static/media/5.2835396c.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "./static/media/6.565ad4a3.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "./static/media/7.fe349e85.jpeg"
+  },
+  {
+    "revision": "f3fa5bc3a5203d928b1629d1784e6920",
+    "url": "./static/media/8.f3fa5bc3.jpeg"
+  },
+  {
+    "revision": "d4a1610c1187e1ee5c39179d29342367",
+    "url": "./static/media/9.d4a1610c.jpeg"
+  },
+  {
+    "revision": "29767d1a8414e8e3d5dee0923dd16c3f",
+    "url": "./index.html"
+  }
+];
